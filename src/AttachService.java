8e1ff3db67bf44e8405f3c12f068dee49284be65
JavokhirJava <abdinabiyev2001@gmail.com>
2023-05-13 21:35:26 +0500

    test 1-2

--- src/AttachService.java
+++ src/AttachService.java
@@ -1,6 +1,13 @@
 public class AttachService {
+    private Integer id;
     private Integer size;
     public void test(){
+        System.out.println("Test");
+    }
+    public void test1(){
+        System.out.println("Test");
+    }
+    public void test2(){
         System.out.println("Test");
     }
 
